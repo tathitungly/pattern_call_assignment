5e2b82315c8218b36f1c85c7a3e26305e3de045d
tathitungly <ly.ta@outlook.com>
2017-03-07 22:03:55 +0100

    Add menu selection to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
 #include "pattern.h"
+
+using namespace std;
 
 int main( int argc, const char* argv[] )
 {
-    Pattern a(9, "test", "test.pat", false);
-    printf( "Leben ist schoen with ID=%d\n", a.getId() );
-
+    bool isContinue = true;
+    string inputStr = "";
+    while (isContinue) {
+        cout << "Hello! What would you like to do?" << endl
+                  << endl
+                  << "1. Enter a new pattern call" << endl
+                  << "2. Delete a pattern call" << endl
+                  << "3. List all pattern calls with a specified name" << endl
+                  << "4. List all pattern calls with a specified path" << endl
+                  << "5. List all pattern calls which are skipped" << endl
+                  << "6. List all pattern calls which are not skipped" << endl
+                  << "7. Exit" << endl << endl;
+        cout << "Your choice? ";
+        getline(cin, inputStr);
+        int choice = atoi(inputStr.c_str());
+        if (choice == 7) {
+            break;
+        }
+        cout << endl << endl;
+    }
 }
